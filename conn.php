65ec309562cb0fb5bdf82a82cd67c3c799177e3e
dev-yohannes <yohannesmulu.gm.dev@gmail.com>
2025-01-06 15:02:58 +0300

    adding connection to the database

---
+++ conn.php
@@ -0,0 +1,11 @@
+<?php
+
+$conn_hostname = "localhost";
+$conn_username = "root";
+$conn_password = "";
+
+$conn = new mysqli($conn_hostname, $conn_username, $conn_password);
+
+if ($conn->connect_error) {
+    die("Connection Faild");
+}
